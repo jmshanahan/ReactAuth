58795e93043097206604092abfccd7c4276db466
jmshanahan <comeraghsolutions@gmail.com>
2023-04-12 09:52:24 +1000

    Auth create signin action completed

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,38 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode" }, { status: 422 });
+  }
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+  const url = `http://localhost:8080/${mode}`;
+
+  const response = await fetch(url, {
+    method: request.method,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(authData),
+  });
+
+  const { status } = response;
+  if (status === 422 || status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user" }, { status: 500 });
+  }
+  return redirect("/");
+}
 export default AuthenticationPage;
